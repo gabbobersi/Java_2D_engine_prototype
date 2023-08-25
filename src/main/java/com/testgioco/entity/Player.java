14e663dfed61b56ed63ec869c7d593119505c948
GiamT <giammarco.marinelli@gmail.com>
2023-08-25 16:55:21 +0200

    Classe Player aggiornata con alcuni valori

--- src/main/java/com/testgioco/entity/Player.java
+++ src/main/java/com/testgioco/entity/Player.java
@@ -1,4 +1,25 @@
 package com.testgioco.entity;
+import com.testgioco.base_classes.Entity;
 
-public class Player {
+public class Player extends Entity {
+    public Player() {
+        // Impostazione dei valori di default
+        this.name = "Gnome";
+        this.type = "Giocatore";
+        this.x = 0;
+        this.y = 0;
+        this.speed = 10;
+        this.life = 100.0;
+
+    }
+
+    // Metodo per impostare i valori di default
+    public void setDefaultValues() {
+        this.name = "Gnome";
+        this.type = "Giocatore";
+        this.x = 0;
+        this.y = 0;
+        this.speed = 10;
+        this.life = 100.0;
+    }
 }
