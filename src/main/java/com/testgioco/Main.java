79722363944a9ab227f892caaa02066df610c225
gabbobersi <gabboberselli@gmail.com>
2023-08-26 17:32:10 +0200

    Correzioni

--- src/main/java/com/testgioco/Main.java
+++ src/main/java/com/testgioco/Main.java
@@ -1,9 +1,20 @@
 package com.testgioco;
 
+import com.testgioco.entity.Player;
+
 public class Main {
     public static void main (String[] args){
         System.out.println("Gioco iniziato");
         new Game();
         System.out.println("Gioco terminato corretamente.");
+
+        Player player1 = new Player();
+        Player player2 = new Player("pippo", "Giocatore", 50, 50, 10, 100);
+        System.out.println(player1.name);
+        System.out.println(player2.name);
+
     }
+
+
+
 }
--- src/main/java/com/testgioco/entity/Player.java
+++ src/main/java/com/testgioco/entity/Player.java
@@ -2,15 +2,16 @@ package com.testgioco.entity;
 import com.testgioco.base_classes.Entity;
 
 public class Player extends Entity {
-    public Player() {
-        // Impostazione dei valori di default
-        this.name = "Gnome";
-        this.type = "Giocatore";
-        this.x = 0;
-        this.y = 0;
-        this.speed = 10;
-        this.life = 100.0;
-
+    public Player(){
+        this.setDefaultValues();
+    }
+    public Player(String name, String type, int x, int y, int speed, double life) {
+        this.name = name;
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.life = life;
     }
 
     // Metodo per impostare i valori di default
