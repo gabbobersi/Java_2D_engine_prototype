7f7e6c2d8e0b2f3839dbad370da2379010890819
GiamT <giammarco.marinelli@gmail.com>
2023-10-24 22:40:17 +0200

    Deleted branch tmapgen

--- src/main/java/com/testgioco/core/Main.java
+++ src/main/java/com/testgioco/core/Main.java
@@ -4,10 +4,10 @@ import com.tilemapgenerator.TileMapGenerator;
 
 public class Main {
     public static void main (String[] args){
-//        Game game = new Game();
-//        game.start();
+      Game game = new Game();
+      game.start();
 
-        TileMapGenerator generator = new TileMapGenerator();
-        generator.run();
+//        TileMapGenerator generator = new TileMapGenerator();
+//        generator.run();
     }
 }
