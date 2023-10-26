1316562f33fbd939c5ce85dae50256bcffcd6abc
gabbobersi <gabboberselli@gmail.com>
2023-10-26 19:01:16 +0200

    Label - Basic label implementation

--- src/main/java/com/testgioco/core/ui_elements/Label.java
+++ src/main/java/com/testgioco/core/ui_elements/Label.java
@@ -1,7 +1,25 @@
 package com.testgioco.core.ui_elements;
 
-public class Label {
-    public Label(){
-        // Sono un costruttore
+import com.testgioco.core.Vector2D;
+
+import javax.swing.*;
+
+public class Label extends JLabel {
+    private Vector2D vector;
+    private String text;
+
+    public Label(String text, int x, int y){
+        super(text);
+        this.text = text;
+        vector.setX(x);
+        vector.setY(y);
     }
+
+    public Label(String text, Vector2D vector){
+        super(text);
+        this.text = text;
+        this.vector = vector;
+    }
+
+
 }
