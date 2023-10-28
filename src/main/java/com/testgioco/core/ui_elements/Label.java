9f72a0221e71fb156af0b8839754c92474de6d83
gabbobersi <gabboberselli@gmail.com>
2023-10-28 23:04:57 +0200

    Added Vector2dInt for static UI elements. Added functioning Label class

--- src/main/java/com/testgioco/core/ui_elements/Label.java
+++ src/main/java/com/testgioco/core/ui_elements/Label.java
@@ -1,6 +1,6 @@
 package com.testgioco.core.ui_elements;
 
-import com.testgioco.core.Vector2D;
+import com.testgioco.core.Vector2DInt;
 import com.testgioco.core.exceptions.UnexpectedStringValueException;
 
 import javax.swing.*;
@@ -10,88 +10,93 @@ import java.awt.*;
  * This class allows to draw and update text on the screen.
  * */
 public class Label  {
-    private final JLabel label = new JLabel();
-    public enum Alignment {
-        LEFT, CENTER, RIGHT
-    }
+    private Font font = new Font("Comic Sans", Font.PLAIN, 15);
+    private Vector2DInt vector = new Vector2DInt(0, 0);
+    private String text = "";
+    private Color fontColor = Color.WHITE;
 
     /**
      * Custom label with coordinates (x, y) for position.
+     * @param x X position.
+     * @param y Y position.
+     * @param text Text of the label.
+     * @param font Font object represent text's font and text's size.
+     * @param fontColor Color of the text.
      * */
-    public Label(int x, int y, String text, Alignment horizontalAlign, Alignment verticalAlign, Font font,
-                 Color fontColor){
-        Vector2D vector = new Vector2D(x, y);
-        setLabel(vector, text, horizontalAlign, verticalAlign, font, fontColor);
+    public Label(int x, int y, String text, Font font, Color fontColor){
+        assignConstructorParametersToInstance(new Vector2DInt(x, y), text, font, fontColor);
     }
 
     /**
      * Custom label with vector for position.
+     * @param vector Vector 2D containing x, y position.
+     * @param text Text of the label.
+     * @param font Font object represent text's font and text's size.
+     * @param fontColor Color of the text.
      * */
-    public Label(Vector2D vector, String text, Alignment horizontalAlign, Alignment verticalAlign, Font font, Color fontColor){
-        setLabel(vector, text, horizontalAlign, verticalAlign, font, fontColor);
+    public Label(Vector2DInt vector, String text, Font font, Color fontColor){
+        assignConstructorParametersToInstance(vector, text, font, fontColor);
     }
 
     /**
      * Default label with vector for position.
+     * @param vector Vector 2D containing x, y position.
+     * @param text Text of the label.
      * */
-    public Label(Vector2D vector, String text){
+    public Label(Vector2DInt vector, String text){
         Font font = new Font("Comic Sans", 1, 30);
-        setLabel(vector, text, Alignment.CENTER, Alignment.CENTER, font, Color.BLACK);
+        assignConstructorParametersToInstance(vector, text, font, Color.BLACK);
     }
 
     /**
-     * Set all label's attributes
-     * @param pos Label's position on the screen.
-     * @param text Text of the label
-     * @param x Horizontal alignment.
-     * @param y Vertical alignment.
-     * @param font Font object. Its size will determine label's font size.
-     * @param color Font's color.
+     * Default labels
      * */
-    private void setLabel(Vector2D pos, String text, Alignment x, Alignment y, Font font, Color color){
-        label.setText(text);
-        label.setLocation((int)pos.getX(), (int)pos.getY());
-        label.setForeground(color);
-        label.setSize(font.getSize(), font.getSize());
-        label.setFont(font);
-
-        try{
-            setAlignment(label, "horizontal", x);
-            setAlignment(label, "vertical", y);
-        } catch (UnexpectedStringValueException e){
-            e.printStackTrace();
-        }
-        label.setVisible(true);
-        label.setBounds(200, 200, label.getPreferredSize().width, label.getPreferredSize().height);
-        reDrawLabel(label);
-    }
-
-    private void reDrawLabel(JLabel lbl){
-        lbl.revalidate();
-        lbl.repaint();
-    }
-
-    private void setAlignment(JLabel lbl, String alignmentType, Alignment alignment) throws UnexpectedStringValueException {
-        if (alignmentType == null || !alignmentType.equals("horizontal") && !alignmentType.equals("vertical")){
-            throw new UnexpectedStringValueException("alignmentType value is not equal 'horizontal' or 'vertical'");
-        }
-
-        switch (alignment) {
-            case LEFT -> lbl.setHorizontalAlignment(SwingConstants.LEFT);
-            case CENTER -> lbl.setHorizontalAlignment(SwingConstants.CENTER);
-            case RIGHT -> lbl.setHorizontalAlignment(SwingConstants.RIGHT);
-        }
+    public Label(){
+        Font font = new Font("Comic Sans", 1, 30);
+        assignConstructorParametersToInstance(new Vector2DInt(100, 100), "", font, Color.BLACK);
     }
 
     /**
-     * Returns the JLabel internal implementation.
-     * It should be used just to add the component to a container.
+     * Assign constructors parameters to instance's attributes.
+     * @param vector Vector 2D containing x, y position.
+     * @param text Text of the label.
+     * @param font Font object represent text's font and text's size.
+     * @param fontColor Color of the text.
      * */
-    public JLabel getComponent(){
-        return label;
+    private void assignConstructorParametersToInstance(Vector2DInt vector, String text, Font font, Color fontColor){
+        this.vector = vector;
+        this.text = text;
+        this.font = font;
+        this.fontColor = fontColor;
     }
 
     public void draw(Graphics2D g2){
-        label.paint(g2);
+        g2.setFont(font);
+        g2.setColor(fontColor);
+        g2.drawString(text, vector.getX(), vector.getY());
+    }
+
+    public Font getFont() {
+        return font;
+    }
+
+    public void setFont(Font font) {
+        this.font = font;
+    }
+
+    public Vector2DInt getVector() {
+        return vector;
+    }
+
+    public void setVector(Vector2DInt vector) {
+        this.vector = vector;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
--- src/main/java/com/testgioco/core/ui_elements/Panel.java
+++ src/main/java/com/testgioco/core/ui_elements/Panel.java
@@ -1,7 +1,7 @@
 package com.testgioco.core.ui_elements;
 
 import com.testgioco.core.Grid;
-import com.testgioco.core.Vector2D;
+import com.testgioco.core.Vector2DInt;
 import com.testgioco.utilities.GameSettings;
 import com.testgioco.utilities.ScreenLogger;
 import com.testgioco.core.InputHandler;
@@ -20,14 +20,22 @@ public class Panel extends JPanel {
     public final Player player = new Player();
     public final ScreenLogger debug = new ScreenLogger();
 
+    private Label label1 = new Label(200, 400, "Test", new Font("Comic Sans", Font.PLAIN, 30), Color.BLACK);
+    private Label label = new Label(new Vector2DInt(100, 100), "Test nuovo daje");
+
     public Panel(InputHandler keyH, Color backGround){
         this.keyH = keyH;
         player.keyH = keyH;
-        setBackground(backGround);
+        setBackground(Color.WHITE);
         addKeyListener(this.keyH);
         setDoubleBuffered(true);
         setFocusable(true);
-        // setPreferredSize(new Dimension(settings.screenWidth, settings.screenHeight));
+        setPreferredSize(new Dimension(settings.screenWidth, settings.screenHeight));
+
+        setLayout(null);
+
+        label.setVector(new Vector2DInt(100, 100));
+        label1.setVector(new Vector2DInt(200, 400));
     }
 
     @Override
@@ -35,10 +43,12 @@ public class Panel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
         // Objects to draw, in order.
-        tileManager.draw(g2);
-        grid.drawDebugGrid(g2);
-        debug.draw(g2);
-        player.draw(g2);
+//        tileManager.draw(g2);
+//        grid.drawDebugGrid(g2);
+//        debug.draw(g2);
+//        player.draw(g2);
+        label.draw(g2);
+        label1.draw(g2);
         g2.dispose();
     }
 }
