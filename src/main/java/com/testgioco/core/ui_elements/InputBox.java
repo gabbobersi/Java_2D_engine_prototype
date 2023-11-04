6b1a7712444d4c937da750295266c6a3fc3eb976
GiamT <giammarco.marinelli@gmail.com>
2023-11-04 16:55:15 +0100

    Modified switch_break_continue

---
+++ src/main/java/com/testgioco/core/ui_elements/InputBox.java
@@ -0,0 +1,18 @@
+package com.testgioco.core.ui_elements;
+
+import javax.swing.*;
+
+public class JTextField extends JFrame {
+    //Attributi
+    private javax.swing.JTextField provaCampoDiTesto;
+
+    //Costruttore
+    public JTextField(){
+
+        }
+
+    //Metodi
+    
+
+
+}
