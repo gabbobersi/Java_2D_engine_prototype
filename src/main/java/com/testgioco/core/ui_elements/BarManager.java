01a51ffe29b2c58aae7988cbeb01b71f4e8e390e
GiamT <giammarco.marinelli@gmail.com>
2023-11-16 12:07:12 +0100

    Modified class BarManager, it's not over yet

---
+++ src/main/java/com/testgioco/core/ui_elements/BarManager.java
@@ -0,0 +1,37 @@
+package com.testgioco.core.ui_elements;
+
+import javax.swing.*;
+
+// Attributi di classe
+public class BarManager extends JFrame {
+    /**
+     * Classe che implementa le barre della vita e della stamina, in futuro si vedrà
+     * se tenerle qui o fare delle barre separate
+     */
+
+    private JProgressBar healthBar;
+    private JProgressBar staminaBar;
+
+    // Costruttori
+    public BarManager(){
+        setTitle("Health");
+        setSize(300, 150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setTitle("Stamina");
+        setSize(300, 150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        // Creazione barre
+        healthBar = new JProgressBar(0,100);
+        healthBar.setValue(100);
+        healthBar.setStringPainted(true);
+
+        staminaBar = new JProgressBar(0, 100);
+        staminaBar.setValue(100);
+        staminaBar.setStringPainted(true);
+
+    }
+}
