0c8c31bc04ddda706f5baf25275029064fb17704
GiamT <giammarco.marinelli@gmail.com>
2023-11-21 15:40:29 +0100

    Updated BarManager, work in progress

--- src/main/java/com/testgioco/core/ui_elements/BarManager.java
+++ src/main/java/com/testgioco/core/ui_elements/BarManager.java
@@ -1,36 +1,47 @@
 package com.testgioco.core.ui_elements;
-
 import javax.swing.*;
+import java.awt.*;
 
-// Attributi di classe
+
+/**
+ * Da inserire nelle classi apposite
+ */
 public class BarManager extends JFrame {
-    /**
-     * Classe che implementa le barre della vita e della stamina, in futuro si vedrà
-     * se tenerle qui o fare delle barre separate
-     */
-
     private JProgressBar healthBar;
     private JProgressBar staminaBar;
 
-    // Costruttori
-    public BarManager(){
-        setTitle("Health");
+    public BarManager() {
+        // Impostazioni della finestra
+        setTitle("Health and Stamina Bar");
         setSize(300, 150);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        setTitle("Stamina");
-        setSize(300, 150);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-
-        // Creazione barre
-        healthBar = new JProgressBar(0,100);
+        // Creazione delle barre
+        healthBar = new JProgressBar(0, 100);
         healthBar.setValue(100);
         healthBar.setStringPainted(true);
 
         staminaBar = new JProgressBar(0, 100);
         staminaBar.setValue(100);
         staminaBar.setStringPainted(true);
+
+        // Pannello per contenere le barre (Da inserire nella classe apposita)
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2, 1));
+        panel.add(healthBar);
+        panel.add(staminaBar);
+
+        // Aggiunta del pannello alla finestra
+        add(panel);
+    }
+
+    // Metodi
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            BarManager bar = new BarManager();
+            bar.setVisible(true);
+        });
     }
 }
