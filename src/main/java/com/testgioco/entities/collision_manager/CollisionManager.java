0626ded485eb289d7d9bb1e494775e67176e15c3
Francesco.Castaldi <290354@studenti.unimore.it>
2023-11-25 11:20:57 +0100

    Added some docstring and explain code for the future :), thanks for all <3

--- src/main/java/com/testgioco/entities/collision_manager/CollisionManager.java
+++ src/main/java/com/testgioco/entities/collision_manager/CollisionManager.java
@@ -24,6 +24,24 @@ public class CollisionManager {
         this.inputHandler = inputHandler;
     }
 
+    /**
+     This code snippet defines a method called canMove() that determines whether
+     the player can move in a certain direction based on input from an inputHandler.
+     It calculates the future player position (futureX and futureY) based on the current position,
+     speed, and the input received. It then calculates the future tile position
+     (futureTileColumn and futureTileRow) based on the future player
+     position and the size of the tiles. Finally, it calls another
+     method canMoveToTile()
+     to check if the movement
+     will collide with a wall, and returns the result.
+     */
+
+    /**
+     * Determines if the player can move in the specified direction.
+     *
+     * @return  true if the player can move, false otherwise.
+     */
+
     public boolean canMove() {
         int futureX = player.positionOnTheMap.getX();
         int futureY = player.positionOnTheMap.getY();
@@ -48,6 +66,31 @@ public class CollisionManager {
         // Controlla se il movimento colpisce un muro
         return canMoveToTile(futureTileRow, futureTileColumn, futureX, futureY);
     }
+
+
+
+
+
+    /*
+     This code snippet defines a method called canMoveToTile that checks
+     if a player can move to a specific tile on a game map.
+     It first checks if the tile coordinates are within the bounds of the game map.
+     If not, it returns false. Next, it checks if the tile at the given coordinates is a wall tile.
+     If it is, it calculates the width and height of the player and checks
+     if the player's future position overlaps with the wall tile.
+     Finally, it returns true if the
+     player can move to the tile
+     (i.e., it does not collide with a wall), and false otherwise.
+     */
+    /**
+     * Determines if the player can move to the specified tile.
+     *
+     * @param  tileRow     the row of the tile being checked
+     * @param  tileColumn  the column of the tile being checked
+     * @param  futureX     the future x-coordinate of the player
+     * @param  futureY     the future y-coordinate of the player
+     * @return             true if the player can move to the tile, false otherwise
+     */
 
     private boolean canMoveToTile(int tileRow, int tileColumn, int futureX, int futureY) {
         boolean withinBounds = tileRow >= 0 && tileRow < tileManager.mapTileNum.length &&
