add54dc824722816534f98d024bce3213ca1a3c5
gabbobersi <gabboberselli@gmail.com>
2023-12-16 03:06:30 +0100

    Removed LogSystem from main...

--- src/main/java/com/testgioco/core/Main.java
+++ src/main/java/com/testgioco/core/Main.java
@@ -1,22 +1,9 @@
 package com.testgioco.core;
 
 
-import com.testgioco.utilities.LogSystem;
-
-import java.util.logging.Level;
-
 public class Main {
     public static void main (String[] args){
         Game game = new Game();
         game.start();
-        try {
-            LogSystem my_log = new LogSystem( Main.class.getName(), Level.INFO,"log.txt");
-            my_log.logInfo("info message");
-
-
-
-        } catch (Exception e) {
-
-        }
     }
 }
